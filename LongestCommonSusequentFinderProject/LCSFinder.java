699093b7f07c14c90bc9897921685e9b55a04850
Saiteja1214 <2320030023@klh.edu.in>
2024-09-09 20:43:40 +0530

    Add files via upload

---
+++ LongestCommonSusequentFinderProject/LCSFinder.java
@@ -0,0 +1,43 @@
+package LongestCommonSusequentFinderProject;
+
+public class LCSFinder {
+	 public static String findLCS(String s1, String s2) {
+	        int m = s1.length();
+	        int n = s2.length();
+	        int[][] dp = new int[m + 1][n + 1];
+	        for (int i = 1; i <= m; i++) {
+	            for (int j = 1; j <= n; j++) {
+	                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+	                    dp[i][j] = dp[i - 1][j - 1] + 1;
+	                } else {
+	                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+	                }
+	            }
+	        }
+	        StringBuilder lcs = new StringBuilder();
+	        int i = m, j = n;
+	        while (i > 0 && j > 0) {
+	            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+	                lcs.append(s1.charAt(i - 1));
+	                i--;
+	                j--;
+	            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+	                i--;
+	            } else {
+	                j--;
+	            }
+	        }
+	        return lcs.reverse().toString();
+	    } 
+	 public static void main(String[] args) {
+	        String s1 = "abcde";
+	        String s2 = "ace";
+	        System.out.println("LCS of \"" + s1 + "\" and \"" + s2 + "\" is: \"" + findLCS(s1, s2) + "\"");
+	        s1 = "abcdef";
+	        s2 = "fbdamn";
+	        System.out.println("LCS of \"" + s1 + "\" and \"" + s2 + "\" is: \"" + findLCS(s1, s2) + "\"");
+	        s1 = "xyz";
+	        s2 = "pqr";
+	        System.out.println("LCS of \"" + s1 + "\" and \"" + s2 + "\" is: \"" + findLCS(s1, s2) + "\"");
+	    }
+}
